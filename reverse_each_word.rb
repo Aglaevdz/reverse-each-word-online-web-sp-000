c88dc9680ba37032c8767d4e61a2bfda976715f2
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-20 02:58:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence1)
   new_array = sentence1.split(" ")
-  new_array.each {|x| x.reverse!}
+  reversed_array = new_array.each {|x| x.reverse!}
   return reversed_array
 end
